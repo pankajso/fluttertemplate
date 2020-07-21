2d9cc7e3a4b0ac7f820448eec12773b53cfec26c
Pankaj Sonawane <pankajso@gmail.com>
2020-07-21 21:16:24 +0530

    Number picker dialog added

--- lib/views/task_view.dart
+++ lib/views/task_view.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:fluttertemplate/models/task_detail.dart';
-
+import 'package:numberpicker/numberpicker.dart';
 import '../models/task.dart';
 import 'tile_overlay.dart';
 
@@ -12,7 +12,6 @@ class TaskView extends StatelessWidget {
   Widget build(BuildContext context) {
     var scaffold = Scaffold(
       appBar: AppBar(
-        
         title: Text(tasks.username),
       ),
       floatingActionButton: buildFloatingActionButton(context),
@@ -27,30 +26,87 @@ class TaskView extends StatelessWidget {
   FloatingActionButton buildFloatingActionButton(BuildContext context) {
     return FloatingActionButton(
       onPressed: () {
+        int _weight =1;
         showDialog(
             context: context,
             builder: (BuildContext context) {
-              return AlertDialog(
+              return Dialog(
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12)),
-                title: Text("Add Task"),
-                content: TextField(
-                  onChanged: (String value) {
-                    tasks.taskdetails.add(TaskDetail(1, value, 50,0,TaskStatus.pause));
-                  },
+                child: Container(
+                  height: MediaQuery.of(context).size.height/1.5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Add Task'),
+                        ),
+                        TextField(
+                            
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Estimated'),
+                        ),
+                        new ListTile(
+                            leading: Icon(Icons.access_time),
+                            title: new Text(
+                              "$_weight kg",
+                            ),
+                            onTap: () => _weight = _showEstimatedTimePicker(context, _weight),
+                          ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Actual'),
+                        ),
+                        SizedBox(
+                          width: 320.0,
+                          child: RaisedButton(
+                            onPressed: () {Navigator.of(context).pop();},
+                            child: Text(
+                              "Save",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            color: const Color(0xFF1BC0C5),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 320.0,
+                          child: RaisedButton(
+                            onPressed: () {Navigator.of(context).pop();},
+                            child: Text(
+                              "Cancle",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            color: const Color(0xFF1BC0C5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                actions: <Widget>[
-                  FlatButton(
-                      onPressed: () {
-                        // setState(() {
-                        //   todos.add(input);
-                        // });
-                        // createTodos();
-                        Navigator.of(context).pop();
-                      },
-                      child: Text("Add"))
-                ],
+                
               );
+              //   title: Text("Add Task"),
+              //   content: TextField(
+              //     onChanged: (String value) {
+              //       tasks.taskdetails.add(TaskDetail(1, value, 50,0,TaskStatus.pause));
+              //     },
+              //   ),
+              //   actions: <Widget>[
+              //     FlatButton(
+              //         onPressed: () {
+              //           // setState(() {
+              //           //   todos.add(input);
+              //           // });
+              //           // createTodos();
+              //           Navigator.of(context).pop();
+              //         },
+              //         child: Text("Add"))
+              //   ],
+              // );
             });
       },
       child: Icon(
@@ -60,6 +116,22 @@ class TaskView extends StatelessWidget {
     );
   }
 
+_showEstimatedTimePicker(BuildContext context, _weight) {
+  showDialog(
+    context: context,
+    child: new NumberPickerDialog.integer(
+      minValue: 1,
+      maxValue: 150,
+      initialIntegerValue: _weight,
+      // initialValue: _weight,
+      title: new Text("Enter your estimated time for the task"),
+    ),
+  ).then((value) {
+    if (value != null) {
+      return value;
+    }
+  });
+}
   _onTaskTap(BuildContext context, int taskID) {}
   Widget _itemBuilder(BuildContext context, Task task, int index) {
     return GestureDetector(
@@ -80,5 +152,5 @@ class TaskView extends StatelessWidget {
   //   // TODO: implement createState
   //   throw UnimplementedError();
   // }
-  
+
 }
